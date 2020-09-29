04c4de49eb51ffd3f72886ee3b2e168b70c70437
Juan-Pablo Silva <jpsilvapena@gmail.com>
2020-09-29 16:38:21 -0300

    [AC-server] Generalization for test loading

--- autocorrect-server/src/homeworks/homework1.py
+++ autocorrect-server/src/homeworks/homework1.py
@@ -1,40 +1,49 @@
+import numpy as np
+
 from src.errors import Container, InvalidInput
 from src.handler import error, send_results, send_test
 from src.homeworks.tester import numpy_isclose
-from src.homeworks.utils import load_tests_path, test_getter
+from src.homeworks.utils import extract_argument, load_tests_path, test_getter
 
 __tests_data = {}
 __tests_expected = {}
 
 
-def check(question_number, test_number, student_answer):
+def check(question_number, data):
     if not __tests_expected:
         load_tests()
 
     try:
+        test = extract_argument(data, "test")
+        student_answer = extract_argument(data, "student_answer")
+
         value = test_getter(
             question_number,
-            test_number,
+            test,
             __tests_expected,
             "test_output")
+
+        try:
+            status, result, comments = numpy_isclose(value, student_answer)
+        except InvalidInput as e:
+            return error(message=e.msg, code="invalid_input")
+        else:
+            return send_results(status=status, mask=result, comments=comments)
+
     except Container as e:
         return e.data
 
-    try:
-        status, result, comments = numpy_isclose(value, student_answer)
-    except InvalidInput as e:
-        return error(message=e.msg, code="invalid_input")
-    else:
-        return send_results(status=status, mask=result, comments=comments)
 
-
-def get_test(question_number, test_number):
+def get_test(question_number, data):
     if not __tests_data:
         load_tests()
+
     try:
+        test = extract_argument(data, "test")
+
         value = test_getter(
             question_number,
-            test_number,
+            test,
             __tests_data,
             "test_data")
     except Container as e:
@@ -48,6 +57,10 @@ def load_tests():
         "./tests/hw1.json",
         test_data=__tests_data,
         test_expected=__tests_expected)
+
+    for question_id, tests in __tests_expected.items():
+        for test_id, values in tests.items():
+            __tests_expected[question_id][test_id] = np.array(values)
 
 
 def reload_tests():
