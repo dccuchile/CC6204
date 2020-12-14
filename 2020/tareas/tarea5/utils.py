04fe4d15d9602f79e85f2f173e2b25876f5d23cb
mauricio <mau.altgr@hotmail.com>
2020-12-14 14:43:54 -0300

    add new fun

--- 2020/tareas/tarea5/utils.py
+++ 2020/tareas/tarea5/utils.py
@@ -4,3 +4,14 @@ def extract_text_from_set(data):
         for sentence in element[1]:
             sentences.append(sentence)
     return sentences
+
+
+def tokenize_text(texts, tokenizer, counter):
+    tokenized_texts = []
+    for text in tqdm(texts):
+        tokenized_text = tokenizer(text.lower())
+        counter.update(tokenized_text)
+        if tokenized_text[-1] != ".":
+            tokenized_text.append(".")
+        tokenized_texts.append(tokenized_text)
+    return tokenized_texts, counter
