1a265d5377307a67d6cb2c01a57ef6284647d47e
mauricio <mau.altgr@hotmail.com>
2020-12-14 15:43:35 -0300

    add generate_sentence

--- 2020/tareas/tarea5/utils.py
+++ 2020/tareas/tarea5/utils.py
@@ -1,6 +1,8 @@
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 from torch.utils.data import Dataset
+from torch.distributions.categorical import Categorical
 
 
 def extract_text_from_set(data):
@@ -40,14 +42,16 @@ class TextDataset(Dataset):
         return self.n
 
 
-def train_one_epoch(model, dataloader, optimizer, loss_fun, clip_value, device, num_classes=None):
+def train_one_epoch(model, dataloader, optimizer, loss_fun, clip_value, device):
     running_loss = 0
     model.train()
     for idx, batch in enumerate(dataloader):
         x, sl = batch
         x, sl = x.to(device), sl.to(device)
-        if num_classes is not None:
-            one_hot = torch.nn.functional.one_hot(x, num_classes).float()
+        if model.emb_flag:
+            logits = model(x[:, :-1])
+        else:            
+            one_hot = torch.nn.functional.one_hot(x, model.nout).float()
             logits = model(one_hot[:, :-1])
         loss = loss_fun(logits.transpose(1, 2), x[:, 1:])
         optimizer.zero_grad()
@@ -60,15 +64,17 @@ def train_one_epoch(model, dataloader, optimizer, loss_fun, clip_value, device, 
     return running_loss
 
 
-def eval_one_epoch(model, dataloader, loss_fun, device, num_classes=None):
+def eval_one_epoch(model, dataloader, loss_fun, device):
     running_loss = 0
     model.eval()
     with torch.no_grad():
         for idx, batch in enumerate(dataloader):
             x, sl = batch
             x, sl = x.to(device), sl.to(device)
-            if num_classes is not None:
-                one_hot = torch.nn.functional.one_hot(x, num_classes).float()
+            if model.emb_flag:
+                logits = model(x[:, :-1])
+            else:            
+                one_hot = torch.nn.functional.one_hot(x, model.nout).float()
                 logits = model(one_hot[:, :-1])
             loss = loss_fun(logits.transpose(1, 2), x[:, 1:])
             running_loss += loss.item()
@@ -83,3 +89,26 @@ def plot_loss(loss):
     plt.xlabel("epoch")
     plt.ylabel("cross entropy")
     return
+
+
+def generate_sentence(model, init_word, stop_word, encoder, vocab):
+    sentence = []
+    model.to("cpu")
+    model.eval()
+    state = torch.zeros(model.nlayers, 1, model.nh)
+    next_word = word2idx[init_word]
+    x = torch.tensor([next_word], dtype=torch.long).view(1, 1)
+    if not model.emb_flag:
+        x = torch.nn.functional.one_hot(x, model.nout).float()        
+    sentence.append(next_word)
+    with torch.no_grad():
+        while next_word != word2idx[stop_word]:
+            logits, state = model.evaluate(x, state)
+            next_word = Categorical(logits=logits).sample().item()
+            sentence.append(next_word)
+            x = torch.tensor([next_word], dtype=torch.long).view(1, 1)
+            if not model.emb_flag:
+                x = torch.nn.functional.one_hot(x, model.nout).float() 
+    sentence = [vocab[idx] for idx in sentence]
+    sentence = " ".join(sentence)
+    return sentence
